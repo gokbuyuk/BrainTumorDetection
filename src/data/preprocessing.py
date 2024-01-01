162fdd46ecbdd7b686fb9a9ccc83930256f9c230
gokbuyuk <gokcenbuyukbas@gmail.com>
2024-01-02 00:13:50 +0300

    modified the preprocessing.py to use the class for preprocessing steps

--- src/data/preprocessing.py
+++ src/data/preprocessing.py
@@ -1,20 +1,15 @@
 import os
-
-print('---- Working directory is:')
-print(os.getcwd())
-
 import sys
-# sys.path.append("../libs")
+sys.path.append(os.getcwd())
 
 import cv2
 import numpy as np
 from icecream import ic
 import matplotlib.pyplot as plt
-# TODO: fix import from libs.utils import *
-from libs.utils import *
+from libs.preprocessor import Preprocessor
 
-# Input paths
-INPUT_PATH = 'data/raw'
 
-crop_black_frame_in_directory(INPUT_PATH, INPUT_PATH.replace('raw', 'interim/cropped'))
-downsample_images('data/interim/cropped', 'data/interim/resized')
+INPUT_PATH = 'data/raw' # Input path
+preprocessor = Preprocessor()
+preprocessor.process_directory(INPUT_PATH, INPUT_PATH.replace('raw', 'interim/resized'))
+
